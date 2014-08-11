bf3dc008cebea03c571cf972c78f37c6c5676587
arazect <za3a24@yandex.ru>
2014-08-11 20:59:05 +0700

    Added BooleanExtension.cs

---
+++ AndaForceExtensions/Variables/Extension/BooleanExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AndaForceUtils.Variables.Extension
+{
+    public static class BooleanExtension
+    {
+        public static String ToHumanString(this bool value)
+        {
+            return value
+                ? "Yes"
+                : "No";
+        }
+    }
+}
